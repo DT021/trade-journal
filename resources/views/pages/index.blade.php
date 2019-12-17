2bbd645dfecd695ec8fcc695dcfbcfd5d16f64c9
camerongraff <camerongraff@mines.edu>
2019-12-17 11:35:31 -0700

    Added login and register buttons to index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>{{config('app.name')}}</h1>
+        <p>Import and analyze your trades to identify where you are most profitable.</p>
+        <p><a class="btn btn-primary btn-lg" href="/login">Login</a> <a class="btn btn-success btn-lg" href="/register">Register</a></p>
+    </div>
+@endsection

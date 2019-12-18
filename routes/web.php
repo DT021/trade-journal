eb8a9c0157f602afa9374653b3af1b3fa50b70c4
camerongraff <camerongraff@mines.edu>
2019-12-17 22:34:10 -0700

    Required users to authenticate before accessing journal

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/journal', 'PagesController@journal');
+Route::get('/journal', 'PagesController@journal')->middleware('auth');
 

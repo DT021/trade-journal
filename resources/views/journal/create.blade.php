583c2a8ee83fc2ee39f2b56b6f379ea08f2fa9bb
camerongraff <camerongraff@mines.edu>
2019-12-19 18:28:27 -0700

    Changed added container to layout

--- resources/views/journal/create.blade.php
+++ resources/views/journal/create.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Create New Journal Entry</h1>
-        {!! Form::open(['action' => 'JournalEntriesController@store', 'class' => 'mb-5']) !!}
-            <div class="form-group">
-                {{Form::textarea('body', '', ['class' => 'form-control', 'id' => 'summary-ckeditor'])}}
-            </div>
-            {{Form::submit('Create New Entry', ['class'=>'btn btn-primary'])}}
-        {!! Form::close() !!} 
-    </div>
+    <h1>Create New Journal Entry</h1>
+    {!! Form::open(['action' => 'JournalEntriesController@store', 'class' => 'mb-5']) !!}
+        <div class="form-group">
+            {{Form::textarea('body', '', ['class' => 'form-control', 'id' => 'summary-ckeditor'])}}
+        </div>
+        {{Form::submit('Create New Entry', ['class'=>'btn btn-primary'])}}
+    {!! Form::close() !!} 
 @endsection
--- resources/views/journal/index.blade.php
+++ resources/views/journal/index.blade.php
@@ -1,24 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Journal</h1>
-        <a href="/journal/create" class="btn btn-primary mb-2">Create New Entry</a>
-        @if(count($journal_entries) > 0)
-            @foreach($journal_entries as $entry)
-                <div class="card mb-2">
-                    <div class="card-body">
-                        <p class="card-text">{!!$entry->body!!}</p>
-                    <a href="/journal/{{$entry->id}}/edit" class="btn btn-secondary btn-small">Edit</a>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Written on {{$entry->created_at}}</small>
-                    </div>
+    <h1>Journal</h1>
+    <a href="/journal/create" class="btn btn-primary mb-5">Create New Entry</a>
+    @if(count($journal_entries) > 0)
+        @foreach($journal_entries as $entry)
+            <div class="card mb-5">
+                <div class="card-body">
+                    <p class="card-text">{!!$entry->body!!}</p>
+                <a href="/journal/{{$entry->id}}/edit" class="btn btn-secondary btn-small">Edit</a>
                 </div>
-            @endforeach
-            {{$journal_entries->links()}}
-        @else
-            <p>No posts found.</p>
-        @endif
-    </div>  
+                <div class="card-footer">
+                    <small class="text-muted">Written on {{$entry->created_at}}</small>
+                </div>
+            </div>
+        @endforeach
+        {{$journal_entries->links()}}
+    @else
+        <p>No posts found.</p>
+    @endif
 @endsection

5889eff3527ad5dd97bd5679ca6098099cb64e84
camerongraff <camerongraff@mines.edu>
2019-12-19 20:10:16 -0700

    Added delete modal for journal entries

--- public/js/app.js
+++ public/js/app.js
@@ -1886,11 +1886,38 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
-  name: 'journal-entry',
   props: ['id', 'body', 'created_at'],
   data: function data() {
     return {};
+  },
+  methods: {
+    deleteEntry: function deleteEntry() {
+      this.$emit('deleteJournalEntry', this.id);
+    }
   }
 });
 
@@ -37318,29 +37345,33 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "card mb-5" }, [
-    _c("div", { staticClass: "card-body" }, [
-      _vm._m(0),
-      _vm._v(" "),
-      _c("p", { staticClass: "card-text" }, [
-        _c("span", { domProps: { innerHTML: _vm._s(this.body) } })
+  return _c("div", [
+    _c("div", { staticClass: "card mb-5" }, [
+      _c("div", { staticClass: "card-body" }, [
+        _vm._m(0),
+        _vm._v(" "),
+        _c("p", { staticClass: "card-text" }, [
+          _c("span", { domProps: { innerHTML: _vm._s(this.body) } })
+        ]),
+        _vm._v(" "),
+        _c(
+          "a",
+          {
+            staticClass: "btn btn-small btn-secondary",
+            attrs: { href: "/journal/" + this.id + "/edit" }
+          },
+          [_vm._v("Edit")]
+        )
       ]),
       _vm._v(" "),
-      _c(
-        "a",
-        {
-          staticClass: "btn btn-secondary btn-small",
-          attrs: { href: "/journal/" + this.id + "/edit" }
-        },
-        [_vm._v("Edit")]
-      )
+      _c("div", { staticClass: "card-footer" }, [
+        _c("small", { staticClass: "text-muted" }, [
+          _vm._v("Written on " + _vm._s(this.created_at))
+        ])
+      ])
     ]),
     _vm._v(" "),
-    _c("div", { staticClass: "card-footer" }, [
-      _c("small", { staticClass: "text-muted" }, [
-        _vm._v("Written on " + _vm._s(this.created_at))
-      ])
-    ])
+    _vm._m(1)
   ])
 }
 var staticRenderFns = [
@@ -37348,9 +37379,90 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("a", { staticClass: "close", attrs: { href: "" } }, [
-      _c("span", { attrs: { "aria-hidden": "true" } }, [_vm._v("×")])
-    ])
+    return _c(
+      "button",
+      {
+        staticClass: "close",
+        attrs: {
+          type: "button",
+          "data-toggle": "modal",
+          "data-target": "#deleteModal"
+        }
+      },
+      [_c("span", { attrs: { "aria-hidden": "true" } }, [_vm._v("×")])]
+    )
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c(
+      "div",
+      {
+        staticClass: "modal fade",
+        attrs: {
+          id: "deleteModal",
+          tabindex: "-1",
+          role: "dialog",
+          "aria-labelledby": "deleteModalLabel",
+          "aria-hidden": "true"
+        }
+      },
+      [
+        _c(
+          "div",
+          { staticClass: "modal-dialog", attrs: { role: "document" } },
+          [
+            _c("div", { staticClass: "modal-content" }, [
+              _c("div", { staticClass: "modal-header" }, [
+                _c(
+                  "h5",
+                  {
+                    staticClass: "modal-title",
+                    attrs: { id: "exampleModalLabel" }
+                  },
+                  [_vm._v("Delete this journal entry?")]
+                ),
+                _vm._v(" "),
+                _c(
+                  "button",
+                  {
+                    staticClass: "close",
+                    attrs: {
+                      type: "button",
+                      "data-dismiss": "modal",
+                      "aria-label": "Close"
+                    }
+                  },
+                  [
+                    _c("span", { attrs: { "aria-hidden": "true" } }, [
+                      _vm._v("×")
+                    ])
+                  ]
+                )
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "modal-footer" }, [
+                _c(
+                  "button",
+                  {
+                    staticClass: "btn btn-dark",
+                    attrs: { type: "button", "data-dismiss": "modal" }
+                  },
+                  [_vm._v("Cancel")]
+                ),
+                _vm._v(" "),
+                _c(
+                  "button",
+                  { staticClass: "btn btn-danger", attrs: { type: "button" } },
+                  [_vm._v("Delete")]
+                )
+              ])
+            ])
+          ]
+        )
+      ]
+    )
   }
 ]
 render._withStripped = true

23dd717c2a26b676f05dd5b394fecb9c98518819
camerongraff <camerongraff@mines.edu>
2019-12-19 17:03:10 -0700

    Added create view for journal entries

---
+++ resources/views/journal/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Create New Journal Entry</h1>
+        {!! Form::open(['action' => 'JournalEntriesController@store', 'class' => 'mb-5']) !!}
+            <div class="form-group">
+                {{Form::textarea('body', '', ['class' => 'form-control', 'id' => 'summary-ckeditor'])}}
+            </div>
+            {{Form::submit('Create New Entry', ['class'=>'btn btn-primary'])}}
+        {!! Form::close() !!} 
+    </div>
+@endsection

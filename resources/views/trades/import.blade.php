300554e1ac2a195b12aaa3cb621aecc5d9ebcf2e
camerongraff <camerongraff@mines.edu>
2019-12-20 20:48:03 -0700

    Added Trades model and controller

---
+++ resources/views/trades/import.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Import Trades</h1>
+<form>
+    <div class="form-group">
+      <label for="exampleFormControlFile1">Upload File</label>
+      <input type="file" class="form-control-file" id="exampleFormControlFile1">
+    </div>
+</form>
+@endsection
